37534b11a0d73733a35b4df4d6802796ae7f15fc
asglasby <aishaextra@gmail.com>
2016-10-20 16:17:45 -0400

    Completed the Add and Display Functions

--- TodoListTutorial/main.js
+++ TodoListTutorial/main.js
@@ -1,1 +1,20 @@
-﻿
+﻿"use strict";
+
+var myTodoItems = ["Study JavaScript", "Walk the Dog", "Practice Coding"];
+
+var addItem = function () {
+    var addTodoItem = document.getElementById('todoItem').value;
+    myTodoItems.push(addTodoItem);
+    document.getElementById('todoItem').value = "";
+    displayItems();
+};
+
+var displayItems = function () {
+    document.getElementById('displayItems').innerHTML = "";
+    for (var i = 0; i < myTodoItems.length; i++) {
+        document.getElementById('displayItems').innerHTML += '<div>' + myTodoItems[i] + '</div><div><button class="btn btn-danger" onclick="deleteItem('+i+');">Delete</button>'  
+    }
+
+};
+
+displayItems();
